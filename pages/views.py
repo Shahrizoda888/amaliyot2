5c399e7976991e8e0bf463198b91f2a3c06e5dea
shahrizodasub <shahrizodasubhonova2006@gmail.com>
2021-07-13 00:42:35 +0500

    o'zgarish

--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,8 @@ from django.views.generic import TemplateView
 # Create your views here.
 class HomePageView(TemplateView):
     template_name='home.html'
+
+
+
+class AboutPageView(TemplateView):
+    template_name='about.html'
